67e287f5c80359c527f611c384ca38c9088bc973
tmdgjs9525 <tmdgjs9525@gmail.com>
2025-01-16 18:06:42 +0900

    feature : 로그인 페이지 TextBox 커스텀

---
+++ netflix-opensliver/netflix_opensliver/Units/CustomTextBox.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace netflix_opensliver.Units
+{
+    public partial class CustomTextBox : TextBox
+    {
+        public int CornerRadius
+        {
+            get { return (int)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CornerRadius.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register("CornerRadius", typeof(int), typeof(CustomTextBox), new PropertyMetadata(0));
+
+      
+
+
+        public CustomTextBox()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
